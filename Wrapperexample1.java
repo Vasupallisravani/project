39839a5cc5360818edf7a354eaac480c99ba5ea2
Vasupallisravani <vasupallisravani5@gmail.com>
2024-10-05 09:58:35 +0530

    Add files via upload

---
+++ Wrapperexample1.java
@@ -0,0 +1,9 @@
+public class Wrapperexample1{
+    public static void main(String[]args){
+//converting int into integer
+int a=20;
+integer i=integer.value of(a);//converting int into integer
+integer j=a;//auto boxing now compiler with write integer value of(a) internally
+System.out.println(a+" "+i+" "+j);
+    }
+}
